394c5a562774dec2a2080ead7dbb58fd82611579
ManaalS <55931638+ManaalS@users.noreply.github.com>
2020-06-22 22:39:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function PopUp(){
+        document.getElementById('ac-wrapper').style.display="block"; 
+}
+document.querySelector('.close').addEventListener("click",()=> document.getElementById('ac-wrapper').style.display='none');
+
